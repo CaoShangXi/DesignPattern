a393682f71d4387857c593be0977ff0a0a06ac1a
CaoShangXi <1427026071qq@sina.com>
2021-11-11 23:34:22 +0800

    原型模式

---
+++ DesignPattern.UnitTests/PrototypePatternTest.cs
@@ -0,0 +1,33 @@
+﻿using DesignPattern.PrototypePattern;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DesignPattern.UnitTests
+{
+    /// <summary>
+    /// 原型模式测试
+    /// </summary>
+    [TestClass]
+    public class PrototypePatternTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ShapeCache.LoadCache();
+
+            Shape clonedShape = (Shape)ShapeCache.GetShape("1");
+            Console.WriteLine("Shape : " + clonedShape.GetType());
+
+            Shape clonedShape2 = (Shape)ShapeCache.GetShape("2");
+            Console.WriteLine("Shape : " + clonedShape2.GetType());
+
+            Shape clonedShape3 = (Shape)ShapeCache.GetShape("3");
+            Console.WriteLine("Shape : " + clonedShape3.GetType());
+        }
+    }
+
+    class Emp { 
+    public string Name { get; set; }
+     public   int Age { get; set; }
+    }
+}
---
+++ DesignPattern/PrototypePattern.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.PrototypePattern
+{
+    /// <summary>
+    /// 形状
+    /// </summary>
+    public abstract class Shape : ICloneable
+    {
+        private string id;
+        protected string type;
+
+        public abstract void Draw();
+
+        public new string GetType()
+        {
+            return type;
+        }
+
+        public string GetId()
+        {
+            return id;
+        }
+
+        public void SetId(string id)
+        {
+            this.id = id;
+        }
+        public object Clone()
+        {
+            object clone = null;
+            try
+            {
+                clone = base.MemberwiseClone();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.StackTrace);
+            }
+            return clone;
+        }
+    }
+
+    /// <summary>
+    /// 长方形
+    /// </summary>
+    public class Rectangle : Shape
+    {
+        public Rectangle()
+        {
+            type = "Rectangle";
+        }
+        public override void Draw()
+        {
+            Console.WriteLine("Inside Rectangle::draw() method.");
+        }
+    }
+
+    /// <summary>
+    /// 正方形
+    /// </summary>
+    public class Square : Shape
+    {
+        public Square()
+        {
+            type = "Square";
+        }
+        public override void Draw()
+        {
+            Console.WriteLine("Inside Square::draw() method.");
+        }
+    }
+
+    /// <summary>
+    /// 圆形
+    /// </summary>
+    public class Circle : Shape
+    {
+        public Circle()
+        {
+            type = "Circle";
+        }
+        public override void Draw()
+        {
+            Console.WriteLine("Inside Circle::draw() method.");
+        }
+    }
+
+    public class ShapeCache
+    {
+
+        private static Dictionary<string, Shape> shapeMap
+           = new Dictionary<string,Shape>();
+
+        public static Shape GetShape(string shapeId)
+        {
+            Shape cachedShape = shapeMap[shapeId];
+            return (Shape)cachedShape.Clone();
+        }
+
+        // 对每种形状都运行数据库查询，并创建该形状
+        // shapeMap.Add(shapeKey, shape);
+        // 例如，我们要添加三种形状
+        public static void LoadCache()
+        {
+            Circle circle = new Circle();
+            circle.SetId("1");
+            shapeMap.Add(circle.GetId(), circle);
+
+            Square square = new Square();
+            square.SetId("2");
+            shapeMap.Add(square.GetId(), square);
+
+            Rectangle rectangle = new Rectangle();
+            rectangle.SetId("3");
+            shapeMap.Add(rectangle.GetId(), rectangle);
+        }
+    }
+}
