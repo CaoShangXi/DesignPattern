b709c8d888199c16479375dc960258e095ea6a06
CaoShangXi <1427026071qq@sina.com>
2021-11-12 00:37:12 +0800

    适配器模式

---
+++ DesignPattern.UnitTests/AdapterPatternTest.cs
@@ -0,0 +1,23 @@
+﻿using DesignPattern.AdapterPattern;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DesignPattern.UnitTests
+{
+    /// <summary>
+    /// 适配器模式
+    /// </summary>
+    [TestClass]
+    public class AdapterPatternTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            AudioPlayer audioPlayer = new AudioPlayer();
+            audioPlayer.Play("mp3", "beyond the horizon.mp3");
+            audioPlayer.Play("mp4", "alone.mp4");
+            audioPlayer.Play("vlc", "far far away.vlc");
+            audioPlayer.Play("avi", "mind me.avi");
+        }
+    }
+}
---
+++ DesignPattern/AdapterPattern.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 适配器模式
+/// </summary>
+namespace DesignPattern.AdapterPattern
+{
+    /// <summary>
+    /// 媒体播放器接口
+    /// </summary>
+    public interface IMediaPlayer
+    {
+        void Play(string audioType, string fileName);
+    }
+
+    /// <summary>
+    /// 高级媒体播放器接口
+    /// </summary>
+    public interface IAdvancedMediaPlayer
+    {
+        void PlayVlc(string fileName);
+        void PlayMp4(string fileName);
+    }
+
+    /// <summary>
+    /// Vlc播放器
+    /// </summary>
+    public class VlcPlayer : IAdvancedMediaPlayer
+    {
+        public void PlayVlc(String fileName)
+        {
+            Console.WriteLine("Playing vlc file. Name: " + fileName);
+        }
+        public void PlayMp4(String fileName)
+        {
+            //什么也不做
+        }
+    }
+
+    /// <summary>
+    /// Mp4播放器
+    /// </summary>
+    public class Mp4Player : IAdvancedMediaPlayer
+    {
+        public void PlayVlc(String fileName)
+        {
+            //什么也不做
+        }
+        public void PlayMp4(String fileName)
+        {
+            Console.WriteLine("Playing mp4 file. Name: " + fileName);
+        }
+    }
+    /// <summary>
+    /// 媒体适配器
+    /// </summary>
+    public class MediaAdapter : IMediaPlayer
+    {
+        IAdvancedMediaPlayer advancedMusicPlayer;
+        public MediaAdapter(String audioType)
+        {
+            if (audioType.Equals("vlc", StringComparison.CurrentCultureIgnoreCase))
+            {
+                advancedMusicPlayer = new VlcPlayer();
+            }
+            else if (audioType.Equals("mp4", StringComparison.CurrentCultureIgnoreCase))
+            {
+                advancedMusicPlayer = new Mp4Player();
+            }
+        }
+        public void Play(String audioType, String fileName)
+        {
+            if (audioType.Equals("vlc", StringComparison.CurrentCultureIgnoreCase))
+            {
+                advancedMusicPlayer.PlayVlc(fileName);
+            }
+            else if (audioType.Equals("mp4", StringComparison.CurrentCultureIgnoreCase))
+            {
+                advancedMusicPlayer.PlayMp4(fileName);
+            }
+        }
+    }
+
+    /// <summary>
+    /// 音频播放器
+    /// </summary>
+    public class AudioPlayer : IMediaPlayer
+    {
+        MediaAdapter mediaAdapter;
+        public void Play(String audioType, String fileName)
+        {
+
+            //播放 mp3 音乐文件的内置支持
+            if (audioType.Equals("mp3", StringComparison.CurrentCultureIgnoreCase))
+            {
+                Console.WriteLine("Playing mp3 file. Name: " + fileName);
+            }
+            //mediaAdapter 提供了播放其他文件格式的支持
+            else if (audioType.Equals("vlc", StringComparison.CurrentCultureIgnoreCase)
+               || audioType.Equals("mp4", StringComparison.CurrentCultureIgnoreCase))
+            {
+                mediaAdapter = new MediaAdapter(audioType);
+                mediaAdapter.Play(audioType, fileName);
+            }
+            else
+            {
+                Console.WriteLine("Invalid media. " +
+                   audioType + " format not supported");
+            }
+        }
+    }
+}
