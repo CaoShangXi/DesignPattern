e06d4cf7e5dee5f8019c01f33a31345315d9d36f
CaoShangXi <1427026071qq@sina.com>
2021-11-17 00:42:19 +0800

    过滤器模式

---
+++ DesignPattern.UnitTests/FilterPatternTest.cs
@@ -0,0 +1,56 @@
+﻿using DesignPattern.FilterPattern;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPattern.UnitTests
+{
+    /// <summary>
+    /// 过滤器模式
+    /// </summary>
+    [TestClass]
+    public class FilterPatternTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            List<Person> persons = new List<Person>();
+
+            persons.Add(new Person("Robert", "Male", "Single"));
+            persons.Add(new Person("John", "Male", "Married"));
+            persons.Add(new Person("Laura", "Female", "Married"));
+            persons.Add(new Person("Diana", "Female", "Single"));
+            persons.Add(new Person("Mike", "Male", "Single"));
+            persons.Add(new Person("Bobby", "Male", "Single"));
+
+            ICriteria male = new CriteriaMale();
+            ICriteria female = new CriteriaFemale();
+            ICriteria single = new CriteriaSingle();
+            ICriteria singleMale = new AndCriteria(single, male);
+            ICriteria singleOrFemale = new OrCriteria(single, female);
+            Console.WriteLine("Males: ");
+            PrintPersons(male.MeetCriteria(persons));
+
+            Console.WriteLine("\nFemales: ");
+            PrintPersons(female.MeetCriteria(persons));
+
+            Console.WriteLine("\nSingle Males: ");
+            PrintPersons(singleMale.MeetCriteria(persons));
+
+            Console.WriteLine("\nSingle Or Females: ");
+            PrintPersons(singleOrFemale.MeetCriteria(persons));
+        }
+
+        public static void PrintPersons(List<Person> persons)
+        {
+            foreach (Person person in persons)
+            {
+                Console.WriteLine("Person : [ Name : " + person.GetName()
+                   + ", Gender : " + person.GetGender()
+                   + ", Marital Status : " + person.GetMaritalStatus()
+                   + " ]");
+            }
+        }
+    }
+}
---
+++ DesignPattern/FilterPattern.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 过滤器模式
+/// </summary>
+namespace DesignPattern.FilterPattern
+{
+    /// <summary>
+    /// 人
+    /// </summary>
+    public class Person
+    {
+        /// <summary>
+        /// 姓名
+        /// </summary>
+        private string name;
+        /// <summary>
+        /// 性别
+        /// </summary>
+        private string gender;
+        /// <summary>
+        /// 婚姻状态
+        /// </summary>
+        private string maritalStatus;
+
+        public Person(string name, string gender, string maritalStatus)
+        {
+            this.name = name;
+            this.gender = gender;
+            this.maritalStatus = maritalStatus;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+        public string GetGender()
+        {
+            return gender;
+        }
+        public string GetMaritalStatus()
+        {
+            return maritalStatus;
+        }
+    }
+
+    /// <summary>
+    /// 标准接口
+    /// </summary>
+    public interface ICriteria
+    {
+        /// <summary>
+        /// 符合标准
+        /// </summary>
+        /// <param name="persons">人</param>
+        /// <returns></returns>
+        List<Person> MeetCriteria(List<Person> persons);
+    }
+
+    /// <summary>
+    /// 男性标准
+    /// </summary>
+    public class CriteriaMale : ICriteria
+    {
+        public List<Person> MeetCriteria(List<Person> persons)
+        {
+            List<Person> malePersons = new List<Person>();
+            foreach (Person person in persons)
+            {
+                if (person.GetGender().Equals("MALE", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    malePersons.Add(person);
+                }
+            }
+            return malePersons;
+        }
+    }
+
+    /// <summary>
+    /// 女性标准
+    /// </summary>
+    public class CriteriaFemale : ICriteria
+    {
+        public List<Person> MeetCriteria(List<Person> persons)
+        {
+            List<Person> femalePersons = new List<Person>();
+            foreach (Person person in persons)
+            {
+                if (person.GetGender().Equals("FEMALE", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    femalePersons.Add(person);
+                }
+            }
+            return femalePersons;
+        }
+    }
+
+    /// <summary>
+    /// 单身标准
+    /// </summary>
+    public class CriteriaSingle : ICriteria
+    {
+        public List<Person> MeetCriteria(List<Person> persons)
+        {
+            List<Person> singlePersons = new List<Person>();
+            foreach (Person person in persons)
+            {
+                if (person.GetMaritalStatus().Equals("SINGLE", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    singlePersons.Add(person);
+                }
+            }
+            return singlePersons;
+        }
+    }
+
+    /// <summary>
+    /// AND标准
+    /// </summary>
+    public class AndCriteria : ICriteria
+    {
+        private ICriteria criteria;
+        private ICriteria otherCriteria;
+        public AndCriteria(ICriteria criteria, ICriteria otherCriteria)
+        {
+            this.criteria = criteria;
+            this.otherCriteria = otherCriteria;
+        }
+        public List<Person> MeetCriteria(List<Person> persons)
+        {
+            List<Person> firstCriteriaPersons = criteria.MeetCriteria(persons);
+            return otherCriteria.MeetCriteria(firstCriteriaPersons);
+        }
+    }
+
+    /// <summary>
+    /// OR标准
+    /// </summary>
+    public class OrCriteria:ICriteria
+    {
+   private ICriteria criteria;
+    private ICriteria otherCriteria;
+    public OrCriteria(ICriteria criteria, ICriteria otherCriteria)
+    {
+        this.criteria = criteria;
+        this.otherCriteria = otherCriteria;
+    }
+   public List<Person> MeetCriteria(List<Person> persons)
+    {
+        List<Person> firstCriteriaItems = criteria.MeetCriteria(persons);
+        List<Person> otherCriteriaItems = otherCriteria.MeetCriteria(persons);
+
+        foreach (Person person in otherCriteriaItems)
+        {
+            if (!firstCriteriaItems.Contains(person))
+            {
+                firstCriteriaItems.Add(person);
+            }
+        }
+        return firstCriteriaItems;
+    }
+}
+}
