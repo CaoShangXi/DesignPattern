7259b598162a44def896cca910c99c73c27e012c
CaoShangXi <1427026071qq@sina.com>
2021-11-19 00:36:46 +0800

    外观模式

---
+++ DesignPattern.UnitTests/FacadePatternTest.cs
@@ -0,0 +1,23 @@
+﻿using DesignPattern.FacadePattern;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DesignPattern.UnitTests
+{
+    /// <summary>
+    /// 外观模式测试
+    /// </summary>
+    [TestClass]
+    public class FacadePatternTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ShapeMaker shapeMaker = new ShapeMaker();
+
+            shapeMaker.DrawCircle();
+            shapeMaker.DrawRectangle();
+            shapeMaker.DrawSquare();
+        }
+    }
+}
---
+++ DesignPattern/FacadePattern.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// 外观模式
+/// </summary>
+namespace DesignPattern.FacadePattern
+{
+    public interface IShape
+    {
+        void Draw();
+    }
+    public class Rectangle : IShape
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Rectangle::Draw()");
+        }
+    }
+    public class Square : IShape
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Square::Draw()");
+        }
+    }
+    public class Circle : IShape
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Circle::Draw()");
+        }
+    }
+
+    /// <summary>
+    /// 形状制造者
+    /// </summary>
+    public class ShapeMaker
+    {
+        private IShape circle;
+        private IShape rectangle;
+        private IShape square;
+
+        public ShapeMaker()
+        {
+            circle = new Circle();
+            rectangle = new Rectangle();
+            square = new Square();
+        }
+
+        public void DrawCircle()
+        {
+            circle.Draw();
+        }
+        public void DrawRectangle()
+        {
+            rectangle.Draw();
+        }
+        public void DrawSquare()
+        {
+            square.Draw();
+        }
+    }
+}
