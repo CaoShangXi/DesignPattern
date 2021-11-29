391f3c4c757bd9a0e9da8ccbeb6b1a270265652d
CaoShangXi <1427026071qq@sina.com>
2021-11-30 00:18:30 +0800

    迭代器模式

---
+++ DesignPattern.UnitTests/IteratorPatternTest.cs
@@ -0,0 +1,22 @@
+﻿using DesignPattern.IteratorPattern;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DesignPattern.UnitTests
+{
+    [TestClass]
+    public class IteratorPatternTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            NameRepository namesRepository = new NameRepository();
+
+            for (IIterator iter = namesRepository.GetIterator(); iter.HasNext();)
+            {
+                string name = (string)iter.Next();
+                Console.WriteLine("Name : " + name);
+            }
+        }
+    }
+}
---
+++ DesignPattern/IteratorPattern.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// 迭代器模式
+/// </summary>
+namespace DesignPattern.IteratorPattern
+{
+    /// <summary>
+    /// 迭代器模式
+    /// </summary>
+    public interface IIterator
+    {
+         bool HasNext();
+         object Next();
+    }
+
+    /// <summary>
+    /// 容器接口
+    /// </summary>
+    public interface IContainer
+    {
+        IIterator GetIterator();
+    }
+
+    public class NameRepository:IContainer
+    {
+   public static string[] names = { "Robert", "John", "Julie", "Lora" };
+
+   public IIterator GetIterator()
+    {
+        return new NameIterator();
+    }
+
+    private class NameIterator:IIterator
+    {
+ 
+      int index;
+      public bool HasNext()
+    {
+        if (index < names.Length)
+        {
+            return true;
+        }
+        return false;
+    }
+      public object Next()
+    {
+        if (this.HasNext())
+        {
+            return names[index++];
+        }
+        return null;
+    }
+}
+}
+}
