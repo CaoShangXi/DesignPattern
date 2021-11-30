c486dc4bf54c248c5c0d71b7d6fa7beff1c175db
CaoShangXi <1427026071qq@sina.com>
2021-12-01 00:10:40 +0800

    中介者模式

---
+++ DesignPattern.UnitTests/MediatorPatternTests.cs
@@ -0,0 +1,20 @@
+﻿using DesignPattern.MediatorPattern;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DesignPattern.UnitTests
+{
+    [TestClass]
+    public class MediatorPatternTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            User robert = new User("Robert");
+            User john = new User("John");
+
+            robert.SendMessage("Hi! John!");
+            john.SendMessage("Hello! Robert!");
+        }
+    }
+}
---
+++ DesignPattern/MediatorPattern.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 中介者模式
+/// </summary>
+namespace DesignPattern.MediatorPattern
+{
+    public class ChatRoom
+    {
+        public static void ShowMessage(User user, string message)
+        {
+            Console.WriteLine(DateTime.Now.ToString()
+               + " [" + user.GetName() + "] : " + message);
+        }
+    }
+
+    public class User
+    {
+        private string name;
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
+
+        public User(string name)
+        {
+            this.name = name;
+        }
+
+        public void SendMessage(string message)
+        {
+            ChatRoom.ShowMessage(this, message);
+        }
+    }
+}
