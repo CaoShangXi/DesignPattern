6edb1f16a62ad6b0fbea17a585b8ffcef2b1e1e2
CaoShangXi <1427026071qq@sina.com>
2021-12-01 00:39:00 +0800

    备忘录模式

---
+++ DesignPattern/MenmetoPattern.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 备忘录模式
+/// </summary>
+namespace DesignPattern.MenmetoPattern
+{
+    /// <summary>
+    /// 备忘录类
+    /// </summary>
+    public class Memento
+    {
+        private string state;
+
+        public Memento(string state)
+        {
+            this.state = state;
+        }
+
+        public string GetState()
+        {
+            return state;
+        }
+    }
+
+    public class Originator
+    {
+        private string state;
+
+        public void SetState(string state)
+        {
+            this.state = state;
+        }
+
+        public string GetState()
+        {
+            return state;
+        }
+
+        public Memento SaveStateToMemento()
+        {
+            return new Memento(state);
+        }
+
+        public void GetStateFromMemento(Memento Memento)
+        {
+            state = Memento.GetState();
+        }
+    }
+
+    public class CareTaker
+    {
+        private List<Memento> mementoList = new List<Memento>();
+
+        public void Add(Memento state)
+        {
+            mementoList.Add(state);
+        }
+
+        public Memento Get(int index)
+        {
+            return mementoList[index];
+        }
+    }
+}
